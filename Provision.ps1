4fe1d8d1330426bbeed5b604f53dfc75e6a2b9cd
jswets <jswets@gmail.com>
2026-01-09 15:15:18 -0600

    added .name property to if statement

--- Provision.ps1
+++ Provision.ps1
@@ -1,2 +1,2 @@
-if (Get-OSDCatalogDriverPack -eq $null){iex(irm https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Get-SurfaceDriversv4.ps1)}
+if ($null -eq $(Get-OSDCatalogDriverPack).name){iex(irm https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Get-SurfaceDriversv4.ps1)}
 Deploy-OSDCloud
