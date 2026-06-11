9d0038f24e9986209086e333dcd42742daf17805
jswets <jswets@gmail.com>
2026-06-11 13:30:13 -0500

    updated syntax

--- Provision.ps1
+++ Provision.ps1
@@ -71,5 +71,5 @@ Start-Process powershell.exe -ArgumentList @(
     "-NoProfile", 
     "-ExecutionPolicy Bypass", 
     "-WindowStyle Normal", 
-    "-Command", "iex(irm https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Cloud-Provision.ps1)" -wait -windowstyle normal
-)
+    "-Command `"iex (irm 'https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Cloud-Provision.ps1')`""
+    ) -wait -windowstyle normal
