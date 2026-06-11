6a99fdf34c278f2d739e59287070e3c004134aca
jswets <jswets@gmail.com>
2026-06-11 10:35:59 -0500

    updating provision.ps1 to show the window of cloudprovsion.ps1

--- Provision.ps1
+++ Provision.ps1
@@ -1,1 +1,6 @@
-iex(irm https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Cloud-Provision.ps1)
+#iex(irm https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Cloud-Provision.ps1)
+Start-Process powershell.exe -ArgumentList @(
+    "-NoProfile", 
+    "-ExecutionPolicy Bypass", 
+    "-WindowStyle Normal", 
+    "-Command", "iex(irm https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Cloud-Provision.ps1)" -wait -windowstyle normal
