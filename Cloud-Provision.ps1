455a8ab99547df542c3399f859bd6e5b967c937e
jswets <jswets@gmail.com>
2026-06-15 10:46:53 -0500

    testing reboot steps

--- Cloud-Provision.ps1
+++ Cloud-Provision.ps1
@@ -198,5 +198,5 @@ Write-Host "File 'os-amd64.json' has been created successfully." -ForegroundColo
 
 #if ($null -eq $(Get-OSDCatalogDriverPack).name){iex(irm https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Get-SurfaceDriversv4.ps1)}
 Deploy-OSDCloud
-
+Restart-Computer
 }
