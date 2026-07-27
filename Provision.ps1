bbdee83ab3e833869191db33bc8087c466167f14
jswets <jswets@gmail.com>
2026-07-27 13:58:00 -0500

    Surface 7 testing

--- Provision.ps1
+++ Provision.ps1
@@ -6,3 +6,12 @@ Start-Process powershell.exe -ArgumentList @(
     "-WindowStyle Normal", 
     "-Command `"iex (irm 'https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/Cloud-Provision.ps1')`""
     ) -wait -windowstyle normal
+If ((get-ciminstance -Class "Win32_ComputerSystem").Model -like "*Surface Laptop for Business 7th*") {
+   Start-Process powershell.exe -ArgumentList @(
+    "-NoProfile", 
+    "-ExecutionPolicy Bypass", 
+    "-WindowStyle Normal", 
+    "-Command `"iex (irm 'https://raw.githubusercontent.com/Justin-Swets/OSD/refs/heads/main/OOB-KB5121767.ps1')`""
+    ) -wait -windowstyle normal
+# Do something
+}
